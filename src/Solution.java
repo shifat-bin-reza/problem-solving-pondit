11f44008569e7ff391d66339a50e6140afe89e44
shifat-bin-reza <m.shifatbinreza@gmail.com>
2023-12-27 16:13:41 +0600

    initializing git repository

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 30, 4, 50, 45, 35};
+        System.out.println("Max: " + getMax(array));
+        System.out.println("Second Max: " + getSecondMax(array));
+    }
+
+
+}
